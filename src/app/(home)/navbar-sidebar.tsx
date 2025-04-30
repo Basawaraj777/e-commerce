1308c19d2365908958ac2282598ca4f662ee2c81
Basawaraj777 <basusahu665@gmail.com>
2025-04-30 10:17:50 +0530

    03-home-layout

---
+++ src/app/(home)/navbar-sidebar.tsx
@@ -0,0 +1,64 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import React from "react";
+
+interface NavbarItem {
+  href: string;
+  children: React.ReactNode;
+}
+
+interface Props {
+  items: NavbarItem[];
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const NavbarSidebar = ({ items, open, onOpenChange }: Props) => {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" className="p-0 transition-none">
+        <SheetHeader className="p-4 border-b">
+          <div className="flex items-center">
+            <SheetTitle>Menu</SheetTitle>
+          </div>
+        </SheetHeader>
+        <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+          {items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.href}
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
+            >
+              {item.children}
+            </Link>
+          ))}
+          <div className="border-t">
+            <Link
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              href="/sign-in"
+              onClick={() => onOpenChange(false)}
+            >
+              Log in
+            </Link>
+            <Link
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+              href="/sign-up"
+              onClick={() => onOpenChange(false)}
+            >
+              Start selling
+            </Link>
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default NavbarSidebar;
